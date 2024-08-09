676342654946d0f5107e7997da880a51a447a915
Denger <89358737+zo0mix@users.noreply.github.com>
2024-08-09 15:23:31 -0400

    Main
    Main

---
+++ psych07.bat
@@ -0,0 +1,7 @@
+haxelib set flixel 5.5.0
+haxelib set flixel-addons 3.2.1
+haxelib set flixel-tools 1.5.1
+haxelib set flixel-ui 2.5.0
+haxelib set lime 8.0.1
+haxelib set openfl 9.3.2
+haxelib set SScript 7.7.0
